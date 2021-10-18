7285c0c6c576b4d522bec490aa1f00152d55c186
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2021-10-18 16:47:49 +0100

    created unit test for deduct method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,25 +6,33 @@ describe Oystercard do
     end 
 
     describe '#top_up' do 
-        # one-liner syntax 
-        it { is_expected.to respond_to(:top_up).with(1).argument }
+      # one-liner syntax 
+      it { is_expected.to respond_to(:top_up).with(1).argument }
 
-        it 'balance changes when card is topped up' do 
-            expect{ subject.top_up 10 }.to change{ subject.balance
-            }.by 10
-        end 
+      it 'balance changes when card is topped up' do 
+          expect{ subject.top_up 10 }.to change{ subject.balance
+          }.by 10
+      end 
 
-        # it 'card has a maximum top-up limit of £90' do 
-        # expect{ subject.top_up(91) }.to raise_error {'Error, maximum top-up amount is £90'}
-        # end 
+      # it 'card has a maximum top-up limit of £90' do 
+      # expect{ subject.top_up(91) }.to raise_error {'Error, maximum top-up amount is £90'}
+      # end 
 
-        it 'raises an error when maximum balance is exceeded' do 
-            amount_capacity = Oystercard::AMOUNT_CAPACITY
-            subject.top_up(amount_capacity)
-            expect{ subject.top_up 1 }.to raise_error 'Maximum balance exceeded'
-        end  
+      it 'raises an error when maximum balance is exceeded' do 
+          amount_capacity = Oystercard::AMOUNT_CAPACITY
+          subject.top_up(amount_capacity)
+          expect{ subject.top_up 1 }.to raise_error 'Maximum balance exceeded'
+      end  
+    end
 
-    end 
+    describe 'deduct' do
+      it { is_expected.to respond_to(:deduct).with(1).argument }
+
+      it 'balance changes when money is deducted from card' do
+        expect { subject.deduct 5 }.to change { subject.balance }.by 5
+      end
+    end
+
 
 
 
