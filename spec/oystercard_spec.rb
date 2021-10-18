113283f1f599e96b7b89529a50f01d84692b7785
yasmin <yasmin.jones@digital.justice.gov.uk>
2021-10-18 15:46:43 +0100

    2 unit tests for top_up passes and balance changes when card is topped up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,17 @@ describe Oystercard do
     it 'has a balance of zero' do 
     expect(subject.balance).to eq 0
     end 
+
+    describe '#top_up' do 
+        # one-liner syntax 
+        it { is_expected.to respond_to(:top_up).with(1).argument }
+
+        it 'balance changes when card is topped up' do 
+            expect{ subject.top_up 10 }.to change{ subject.balance
+            }.by 10
+        end 
+    end 
+
+
+
 end 
