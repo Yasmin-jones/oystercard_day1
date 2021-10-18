7a21ce028f6392364a52f4c8c6cd4fb7c85e1140
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2021-10-18 16:50:08 +0100

    minor change to indentation using rubocop

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,19 +1,18 @@
+# frozen_string_literal: true
 
 class Oystercard
+  attr_reader :balance
 
-    attr_reader :balance
+  AMOUNT_CAPACITY = 90
 
-    AMOUNT_CAPACITY = 90
+  def initialize
+    @balance = 0
+  end
 
-    def initialize
-        @balance = 0 
-    end 
+  def top_up(amount)
+    message_error = 'Maximum balance exceeded'
+    raise message_error if amount + balance > AMOUNT_CAPACITY
 
-    def top_up(amount)
-        
-        message_error = 'Maximum balance exceeded'
-        raise message_error if  amount + balance > AMOUNT_CAPACITY
-        @balance += amount
-    end 
-
-end 
+    @balance += amount
+  end
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,39 +1,38 @@
+# frozen_string_literal: true
+
 require 'Oystercard'
 
-describe Oystercard do 
-    it 'has a balance of zero' do 
+describe Oystercard do
+  it 'has a balance of zero' do
     expect(subject.balance).to eq 0
-    end 
+  end
 
-    describe '#top_up' do 
-      # one-liner syntax 
-      it { is_expected.to respond_to(:top_up).with(1).argument }
+  describe '#top_up' do
+    # one-liner syntax
+    it { is_expected.to respond_to(:top_up).with(1).argument }
 
-      it 'balance changes when card is topped up' do 
-          expect{ subject.top_up 10 }.to change{ subject.balance
-          }.by 10
-      end 
-
-      # it 'card has a maximum top-up limit of £90' do 
-      # expect{ subject.top_up(91) }.to raise_error {'Error, maximum top-up amount is £90'}
-      # end 
-
-      it 'raises an error when maximum balance is exceeded' do 
-          amount_capacity = Oystercard::AMOUNT_CAPACITY
-          subject.top_up(amount_capacity)
-          expect{ subject.top_up 1 }.to raise_error 'Maximum balance exceeded'
-      end  
+    it 'balance changes when card is topped up' do
+      expect { subject.top_up 10 }.to change {
+        subject.balance
+      }.by 10
     end
 
-    describe 'deduct' do
-      it { is_expected.to respond_to(:deduct).with(1).argument }
+    # it 'card has a maximum top-up limit of £90' do
+    # expect{ subject.top_up(91) }.to raise_error {'Error, maximum top-up amount is £90'}
+    # end
 
-      it 'balance changes when money is deducted from card' do
-        expect { subject.deduct 5 }.to change { subject.balance }.by 5
-      end
+    it 'raises an error when maximum balance is exceeded' do
+      amount_capacity = Oystercard::AMOUNT_CAPACITY
+      subject.top_up(amount_capacity)
+      expect { subject.top_up 1 }.to raise_error 'Maximum balance exceeded'
     end
+  end
 
+  describe 'deduct' do
+    it { is_expected.to respond_to(:deduct).with(1).argument }
 
-
-
-end 
+    it 'balance changes when money is deducted from card' do
+      expect { subject.deduct 5 }.to change { subject.balance }.by 5
+    end
+  end
+end
